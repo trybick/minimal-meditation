7d68166b995556b959df677a31a344375d53d7da
trybick <t.rybicki@gmail.com>
2022-04-01 16:02:10 -0400

    Add back back button

--- src/components/TimerPage/ButtonControls.tsx
+++ src/components/TimerPage/ButtonControls.tsx
@@ -1,10 +1,20 @@
 import { StyleSheet, View } from 'react-native';
+import { useHistory } from 'react-router-native';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntIcon from 'react-native-vector-icons/AntDesign';
+import { ROUTES } from 'utils/routes';
 import colors from 'style/colors';
 
 export default function ButtonControls() {
+  const history = useHistory();
+
+  const onPressBack = () => {
+    history.push(ROUTES.HOME);
+  };
+
   return (
     <View style={styles.buttonsContainer}>
+      <AntIcon color={colors.primary} name="back" onPress={onPressBack} size={25} />
       <MaterialIcon color={colors.primary} name="pause" size={40} />
     </View>
   );
