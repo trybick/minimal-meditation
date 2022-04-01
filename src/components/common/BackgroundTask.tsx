8ee455a94ff6ca3fa246cb787f5a199fec008eea
trybick <t.rybicki@gmail.com>
2022-04-01 18:45:22 -0400

    Add comment to background task

--- src/components/common/BackgroundTask.tsx
+++ src/components/common/BackgroundTask.tsx
@@ -5,6 +5,8 @@ type Props = {
   functionToRun: () => void;
 };
 
+// This is a hacky way to run a timer in the background. If this stops working, the next best method
+// might be to use the react native background timer package but it requires ejecting.
 function BackgroundTask(props: Props) {
   return (
     <WebView
@@ -17,4 +19,5 @@ function BackgroundTask(props: Props) {
     />
   );
 }
+
 export default BackgroundTask;
