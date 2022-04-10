fc430ff6d6ac5baed93d164e126b0cf4a4cac46e
trybick <t.rybicki@gmail.com>
2022-04-10 15:26:46 -0400

    Fix duration above 60 in timer page

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,11 +1,5 @@
-// example input '10:00'
-export const convertMinutesToSeconds = (duration: string) => {
-  const [minutes, seconds] = duration.split(':');
-  return +minutes * 60 + seconds;
-};
-
 export const convertSecondsToClockTime = (secondsRemaining: number) => {
-  const minutes = Math.floor((secondsRemaining / 60) % 60);
+  const minutes = Math.floor(secondsRemaining / 60);
   const seconds = Math.floor(secondsRemaining % 60);
   const minutesForDisplay = minutes < 10 ? `0${minutes}` : minutes;
   const secondsForDisplay = seconds < 10 ? `0${seconds}` : seconds;
