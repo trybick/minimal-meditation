49611172c54b6b60d59e86f565a3d4a051c26b90
trybick <t.rybicki@gmail.com>
2022-04-16 10:43:02 -0400

    Hide background when dialog is open

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,18 +1,59 @@
+import { useReducer } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useBackButtonHandler } from 'hooks/useBackButtonHandler';
 import Layout from 'components/common/Layout';
 import OptionsContainer from './OptionsContainer';
 import StartButton from './StartButton';
+import DurationOptionsDialog from './Dialogs/DurationOptionsDialog';
+import CustomDurationDialog from './Dialogs/CustomDurationDialog';
+import EndingSoundDialog from './Dialogs/EndingSoundDialog';
+import { Action, DialogName, State } from './OptionsContainerTypes';
+
+const initialState = {
+  currentDialog: null,
+  customDuration: '',
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'openDialog':
+      return { ...state, currentDialog: action.name };
+    case 'closeDialogs':
+      return { ...state, currentDialog: null };
+    case 'setCustomDuration':
+      return { ...state, customDuration: action.value };
+    default:
+      throw new Error('reducer failed');
+  }
+}
 
 export default function HomePage() {
   useBackButtonHandler();
+  const [{ currentDialog, customDuration }, dispatch] = useReducer(reducer, initialState);
+
+  const isDialogVisible = (name: DialogName) => currentDialog === name;
+  const openDialog = (name: DialogName) => dispatch({ type: 'openDialog', name });
+  const closeDialogs = () => dispatch({ type: 'closeDialogs' });
 
   return (
     <Layout>
-      <View style={styles.pageContainer}>
-        <OptionsContainer />
+      <View style={[styles.pageContainer, !!currentDialog && styles.hidden]}>
+        <OptionsContainer openDialog={openDialog} />
         <StartButton />
       </View>
+
+      <DurationOptionsDialog
+        closeDialogs={closeDialogs}
+        isVisible={isDialogVisible('DurationOptions')}
+        openDialog={openDialog}
+      />
+      <CustomDurationDialog
+        closeDialogs={closeDialogs}
+        customDuration={customDuration}
+        dispatch={dispatch}
+        isVisible={isDialogVisible('CustomDuration')}
+      />
+      <EndingSoundDialog closeDialogs={closeDialogs} isVisible={isDialogVisible('EndingSound')} />
     </Layout>
   );
 }
@@ -21,7 +62,11 @@ const styles = StyleSheet.create({
   pageContainer: {
     display: 'flex',
     alignItems: 'center',
+    width: '100%',
     justifyContent: 'center',
     flex: 1,
+  },
+  hidden: {
+    opacity: 0,
   },
 });
--- src/components/HomePage/OptionsContainer.tsx
+++ src/components/HomePage/OptionsContainer.tsx
@@ -1,4 +1,3 @@
-import { useReducer } from 'react';
 import { useRecoilValue } from 'recoil';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { Text } from '@rneui/base';
@@ -7,40 +6,18 @@ import { timerDurationState } from 'state/duration';
 import { endingSoundState } from 'state/settings';
 import { convertSecondsToClockTime } from 'utils/time';
 import colors from 'style/colors';
-import DurationOptionsDialog from './Dialogs/DurationOptionsDialog';
-import CustomDurationDialog from './Dialogs/CustomDurationDialog';
-import EndingSoundDialog from './Dialogs/EndingSoundDialog';
-import { Action, DialogName, State } from './OptionsContainerTypes';
+import { DialogName } from './OptionsContainerTypes';
 
-const initialState = {
-  currentDialog: null,
-  customDuration: '',
-};
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case 'openDialog':
-      return { ...state, currentDialog: action.name };
-    case 'closeDialogs':
-      return { ...state, currentDialog: null };
-    case 'setCustomDuration':
-      return { ...state, customDuration: action.value };
-    default:
-      throw new Error('reducer failed');
-  }
-}
-
-export default function OptionsContainer() {
+export default function OptionsContainer({
+  openDialog,
+}: {
+  openDialog: (name: DialogName) => void;
+}) {
   const timerDuration = useRecoilValue(timerDurationState);
   const endingSound = useRecoilValue(endingSoundState);
-  const [{ currentDialog, customDuration }, dispatch] = useReducer(reducer, initialState);
-
-  const isDialogVisible = (name: DialogName) => currentDialog === name;
-  const openDialog = (name: DialogName) => dispatch({ type: 'openDialog', name });
-  const closeDialogs = () => dispatch({ type: 'closeDialogs' });
 
   return (
-    <View style={styles.optionsContainer}>
+    <View style={styles.pageContainer}>
       <TouchableOpacity
         onPress={() => openDialog('DurationOptions')}
         style={styles.optionContainer}
@@ -59,25 +36,12 @@ export default function OptionsContainer() {
           <Icon color={colors.primary} name="chevron-small-right" size={22} />
         </View>
       </TouchableOpacity>
-
-      <DurationOptionsDialog
-        closeDialogs={closeDialogs}
-        isVisible={isDialogVisible('DurationOptions')}
-        openDialog={openDialog}
-      />
-      <CustomDurationDialog
-        closeDialogs={closeDialogs}
-        customDuration={customDuration}
-        dispatch={dispatch}
-        isVisible={isDialogVisible('CustomDuration')}
-      />
-      <EndingSoundDialog closeDialogs={closeDialogs} isVisible={isDialogVisible('EndingSound')} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  optionsContainer: {
+  pageContainer: {
     display: 'flex',
     alignItems: 'center',
     width: '100%',
